06111abee7aa72bb91f09aff082213c4e251a8e3
Alex Roan <alex.roan@hotmail.com>
2020-08-01 22:47:42 +0100

    Modular, changeable oracle

--- contracts/USM.sol
+++ contracts/USM.sol
@@ -2,9 +2,10 @@ pragma solidity ^0.6.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IOracle.sol";
 
-contract USM is ERC20 {
+contract USM is ERC20, Ownable {
     using SafeMath for uint;
 
     address oracle;
@@ -15,22 +16,39 @@ contract USM is ERC20 {
         ethPool = 0;
     }
 
+    function setOracle(address _oracle) external onlyOwner {
+        oracle = _oracle;
+    }
+
     function mint() external payable returns (uint) {
         require(msg.value > 0, "Must send Ether to mint");
-        uint usdPrice = IOracle(oracle).latestPrice();
-        uint decimalShift = IOracle(oracle).decimalShift();
-        uint usmAmount = usdPrice.mul(msg.value).div(10**decimalShift);
+        uint usmAmount = _ethToUsm(msg.value);
         ethPool = ethPool.add(msg.value);
         _mint(msg.sender, usmAmount);
         return usmAmount;
     }
 
     function burn(uint _usmAmount) external {
-        uint usdPrice = IOracle(oracle).latestPrice();
-        uint decimalShift = IOracle(oracle).decimalShift();
-        uint ethAmount = (_usmAmount.mul(10**decimalShift)).div(usdPrice);
+        uint ethAmount = _usmToEth(_usmAmount);
         ethPool = ethPool.sub(ethAmount);
         _burn(msg.sender, _usmAmount);
         Address.sendValue(msg.sender, ethAmount);
+    }
+
+    function _ethToUsm(uint _ethAmount) internal view returns (uint) {
+        (uint usdPrice, uint decimalShift) = _oracleData();
+        return usdPrice.mul(_ethAmount).div(10**decimalShift);
+    }
+
+    function _usmToEth(uint _usmAmount) internal view returns (uint) {
+        (uint usdPrice, uint decimalShift) = _oracleData();
+        return (_usmAmount.mul(10**decimalShift)).div(usdPrice);
+    }
+
+    function _oracleData() internal view returns (uint, uint) {
+        return (
+            IOracle(oracle).latestPrice(),
+            IOracle(oracle).decimalShift()
+        );
     }
 }
