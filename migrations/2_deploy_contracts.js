0c0f00c27d68df235301cab632c384d65a1f6bd0
Alex Roan <alex.roan@hotmail.com>
2020-08-23 18:38:32 +0100

    Added ChainlinkOracle

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-const USDOracle = artifacts.require("USDOracle");
+const ChainlinkOracle = artifacts.require("ChainlinkOracle");
 const USM = artifacts.require("USM");
 
 module.exports = async function(deployer) {
@@ -6,6 +6,6 @@ module.exports = async function(deployer) {
     // not be undefined??
     await web3.eth.net.getId();
     
-    let oracle = await deployer.deploy(USDOracle);
+    let oracle = await deployer.deploy(ChainlinkOracle, "0x30B5068156688f818cEa0874B580206dFe081a03", 8);
     await deployer.deploy(USM, oracle.address);
 }
