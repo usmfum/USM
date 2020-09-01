15255124ab568c5c470d16fd3b25b2eacf1a483f
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-01 08:56:17 +0100

    Using BN

--- test/02_BufferedToken.test.js
+++ test/02_BufferedToken.test.js
@@ -1,3 +1,5 @@
+const { BN, expectRevert } = require('@openzeppelin/test-helpers');
+
 const TestOracle = artifacts.require("./TestOracle.sol");
 const BufferedToken = artifacts.require("./MockBufferedToken.sol");
 
@@ -11,31 +13,32 @@ contract("BufferedToken", accounts => {
     const [deployer, user] = accounts;
     let token;
 
-    const price = '25000';
-    const decShift = '2';
-    const priceWAD = '250000000000000000000'; // TODO: This should be encoded in a javascript function that combines price and decShift
+    const price = new BN('25000');
+    const shift = new BN('2');
+    const WAD = new BN('1000000000000000000');
+    const priceWAD = price.mul(WAD).div((new BN('10')).pow(shift));
     
     beforeEach(async() => {
-        oracle = await TestOracle.new(price, decShift, {from: deployer});
-        token = await BufferedToken.new(oracle.address, "Name", "Symbol", {from: deployer});
+        oracle = await TestOracle.new(price, shift, { from: deployer });
+        token = await BufferedToken.new(oracle.address, "Name", "Symbol", { from: deployer });
     });
 
     describe("deployment", async () => {
         it("returns the correct price", async () => {
             let oraclePrice = (await oracle.latestPrice()).toString();
-            price.should.equal(oraclePrice);
+            oraclePrice.should.equal(price.toString());
         });
 
         it("returns the correct decimal shift", async () => {
-            let shift = (await oracle.decimalShift()).toString()
-            shift.should.equal(decShift);
+            let decimalshift = (await oracle.decimalShift()).toString()
+            decimalshift.should.equal(shift.toString());
         })
     });
 
     describe("functionality", async () => {
         it("returns the oracle price in WAD", async () => {
             let oraclePrice = (await token.oraclePrice()).toString()
-            priceWAD.should.equal(oraclePrice);
+            oraclePrice.should.equal(priceWAD.toString());
         })
     })
 });
