09127611eddde4745408eb932e713f871d0fe29e
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-11 07:26:56 +0100

    fix: removed unnecessar getters

--- contracts/oracles/ChainlinkOracle.sol
+++ contracts/oracles/ChainlinkOracle.sol
@@ -6,23 +6,19 @@ import "@chainlink/contracts/src/v0.6/interfaces/AggregatorInterface.sol";
 contract ChainlinkOracle is IOracle{
 
     address public oracle;
-    uint private decShift;
+    uint public override decimalShift;
 
     /**
      * @notice Ropsten Details:
      * address: 0x30B5068156688f818cEa0874B580206dFe081a03
      * decShift: 8
      */
-    constructor(address _oracle, uint _decShift) public {
-        oracle = _oracle;
-        decShift = _decShift;
+    constructor(address oracle_, uint shift) public {
+        oracle = oracle_;
+        decimalShift = shift;
     }
 
     function latestPrice() external override view returns (uint) {
         return uint(AggregatorInterface(oracle).latestAnswer());
-    }
-
-    function decimalShift() external override view returns (uint) {
-        return decShift;
     }
 }
