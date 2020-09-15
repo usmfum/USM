be09166d8f10e6c5b3aae2d3e688f7d137d4cc85
Alex Roan <alex.roan@hotmail.com>
2020-09-15 19:37:54 +0100

    Revert "Merged BufferedToken into USM"
    This reverts commit 141a01a1e12f4003f59993b43070f9baa190c690.

---
+++ contracts/mocks/MockBufferedToken.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.6.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "../BufferedToken.sol";
+
+
+/**
+ * @title Mock Buffered Token
+ * @author Alberto Cuesta Cañada (@acuestacanada)
+ * @notice This contract gives access to the internal functions of BufferedToken for testing
+ */
+contract MockBufferedToken is BufferedToken {
+
+    constructor(address _oracle, string memory _name, string memory _symbol) public BufferedToken(_oracle, _name, _symbol) { }
+
+    function mint(uint ethAmount) public returns (uint) {
+        return _mint(ethAmount);
+    }
+
+    function burn(uint usmAmount) public returns (uint) {
+        return _burn(usmAmount);
+    }
+
+    function ethToUsm(uint _ethAmount) public view returns (uint) {
+        return _ethToUsm(_ethAmount);
+    }
+
+    function usmToEth(uint usmAmount) public view returns (uint) {
+        return _usmToEth(usmAmount);
+    }
+
+    function oraclePrice() public view returns (uint) {
+        return _oraclePrice();
+    }
+
+}
