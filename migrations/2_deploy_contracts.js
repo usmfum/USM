b48cc6c638c894c434f5ff2794a3de59fa5774ad
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-09-16 22:51:43 +0100

    feat: use third party testnet oracles

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,33 @@
 const TestOracle = artifacts.require("TestOracle");
+const ChainlinkOracle = artifacts.require("ChainlinkOracle");
 const USM = artifacts.require("USM");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network) {
     // For some reason, this helps `oracle.address`
     // not be undefined??
     await web3.eth.net.getId();
     
-    let oracle = await deployer.deploy(TestOracle, "25000", "2");
-    await deployer.deploy(USM, oracle.address);
+    const oracleAddresses = {
+        'ropsten' : '0x30B5068156688f818cEa0874B580206dFe081a03',
+        'rinkeby' : '0x8A753747A1Fa494EC906cE90E9f37563A8AF630e',
+        'kovan'   : '0x9326BFA02ADD2366b30bacB125260Af641031331'
+    }
+    const wethAddresses = {
+        'ropsten' : 0xc778417e063141139fce010982780140aa0cd5ab,
+        'rinkeby' : 0xc778417e063141139fce010982780140aa0cd5ab,
+        'kovan'   : 0xd0a1e359811322d97991e03f863a0c30c2cf029c
+    }
+
+    let oracle
+    if (network !== 'ropsten' && network !== 'rinkeby' && network !== 'kovan') {
+        await deployer.deploy(TestOracle, "25000", "2");
+        oracle = await TestOracle.deployed()
+    }
+    else {
+        await deployer.deploy(ChainlinkOracle, oracleAddresses[network], "2")
+        oracle = await ChainlinkOracle.deployed()
+    }
+
+    await deployer.deploy(USM, oracle.address/*, wethAddresses[network]*/);
+    const usm = await USM.deployed()
 }
