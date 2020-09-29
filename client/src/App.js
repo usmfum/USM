b78477c2d204d5d260f3526bf84c9734993794c8
Alex Roan <alex.roan@hotmail.com>
2020-09-29 20:19:17 +0100

    Added all four forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,69 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Col, Container, Row, Tab, Tabs} from "react-bootstrap";
+import {Button, Col, Container, Form, Row, Tab, Tabs} from "react-bootstrap";
 import './App.scss';
 
 function App() {
 	return (
 		<Container className="h-100">
 			<Row className="h-100 justify-content-center align-items-center d-flex">
-				<Col sm="4" className="text-center border border-thick">
+				<Col xs="8" sm="6" md="4" className="text-center border border-thick">
 					<Tabs fill className="py-2 ">
 						<Tab eventKey="USM" title="USM" className="">
 							<span>You have 50 USM</span>
-							<Tabs fill>
+							<Tabs fill className="py-2 ">
 								<Tab eventKey="mint" title="Mint">
-									Mint form goes here...
+									<span>Mint USM</span>
+									<Form className="py-2">
+										<Form.Group>
+											<Form.Control type="number" min="0" step=".01" placeholder="ETH" />
+										</Form.Group>
+										<Form.Group>
+											<Form.Control type="number" min="0" step=".01" placeholder="USM" />
+										</Form.Group>
+										<Button type="submit" variant="primary">Go</Button>
+									</Form>
 								</Tab>
 								<Tab eventKey="burn" title="Burn">
-									Burn form goes here...
+									<span>Burn USM</span>
+									<Form className="py-2">
+										<Form.Group>
+											<Form.Control type="number" min="0" step=".01" placeholder="USM" />
+										</Form.Group>
+										<Form.Group>
+											<Form.Control type="number" min="0" step=".01" placeholder="ETH" />
+										</Form.Group>
+										<Button type="submit" variant="primary">Go</Button>
+									</Form>
 								</Tab>
 							</Tabs>
 						</Tab>
 						<Tab eventKey="FUM" title="FUM">
 							<span>You have 75 FUM</span>
-							<Tabs fill>
+							<Tabs fill className="py-2 ">
 								<Tab eventKey="fund" title="Fund">
-									Fund form goes here...
+									<span>Fund with ETH</span>
+									<Form className="py-2">
+										<Form.Group>
+											<Form.Control type="number" min="0" step=".01" placeholder="ETH" />
+										</Form.Group>
+										<Form.Group>
+											<Form.Control type="number" min="0" step=".01" placeholder="FUM" />
+										</Form.Group>
+										<Button type="submit" variant="primary">Go</Button>
+									</Form>
 								</Tab>
 								<Tab eventKey="defund" title="Defund">
-									Defund form goes here...
+									<span>Defund FUM</span>
+									<Form className="py-2">
+										<Form.Group>
+											<Form.Control type="number" min="0" step=".01" placeholder="FUM" />
+										</Form.Group>
+										<Form.Group>
+											<Form.Control type="number" min="0" step=".01" placeholder="ETH" />
+										</Form.Group>
+										<Button type="submit" variant="primary">Go</Button>
+									</Form>
 								</Tab>
 							</Tabs>
 						</Tab>
