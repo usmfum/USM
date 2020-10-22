61abd2ab344994db5190a76bfa7397793945cf34
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-10-22 15:33:21 +0100

    test: ChainlinkAggregator

--- test/01_TestOracle.test.js
+++ test/01_TestOracle.test.js
@@ -1,6 +1,8 @@
 const TestOracle = artifacts.require('TestOracle')
 const Medianizer = artifacts.require('MockMakerMedianizer')
 const MakerOracle = artifacts.require('MakerOracle')
+const Aggregator = artifacts.require('MockChainlinkAggregatorV3')
+const ChainlinkOracle = artifacts.require('ChainlinkOracle')
 
 require('chai').use(require('chai-as-promised')).should()
 
@@ -28,7 +30,7 @@ contract('TestOracle', (accounts) => {
   })
 })
 
-contract('MakerOracle', (accounts) => {
+contract('Maker', (accounts) => {
   const [deployer] = accounts
   let oracle
   let medianizer
@@ -41,6 +43,34 @@ contract('MakerOracle', (accounts) => {
     await medianizer.set(price);
 
     oracle = await MakerOracle.new(medianizer.address, shift, { from: deployer })
+  })
+
+  describe('deployment', async () => {
+    it('returns the correct price', async () => {
+      let oraclePrice = (await oracle.latestPrice())
+      oraclePrice.toString().should.equal(price)
+    })
+
+    it('returns the correct decimal shift', async () => {
+      let oracleShift = (await oracle.decimalShift())
+      oracleShift.toString().should.equal(shift)
+    })
+  })
+})
+
+contract('Chainlink', (accounts) => {
+  const [deployer] = accounts
+  let oracle
+  let aggregator
+
+  const price = '38598000000'
+  const shift = '8'
+
+  beforeEach(async () => {
+    aggregator = await Aggregator.new({ from: deployer })
+    await aggregator.set(price);
+
+    oracle = await ChainlinkOracle.new(aggregator.address, shift, { from: deployer })
   })
 
   describe('deployment', async () => {
