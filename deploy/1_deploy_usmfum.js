19c55c1b8c103eb8e6012d882e9b207aa242ef4c
capu <hi@capu.tech>
2021-01-14 16:34:41 -0300

    linting deployments

--- deploy/1_deploy_usmfum.js
+++ deploy/1_deploy_usmfum.js
@@ -1,41 +1,33 @@
-
 const chainlinkAddresses = {
-  '1' : '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
-  '42' : '0x9326BFA02ADD2366b30bacB125260Af641031331',
+  1: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
+  42: '0x9326BFA02ADD2366b30bacB125260Af641031331',
 }
 const compoundAddresses = {
-  '1' : '0x922018674c12a7f0d394ebeef9b58f186cde13c1',
-  '42' : '0x0000000000000000000000000000000000000000',
-}
-const ethUsdtAddresses = {
-  '1' : '0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852',
-  '42' : '0x0000000000000000000000000000000000000000',
+  1: '0x922018674c12a7f0d394ebeef9b58f186cde13c1',
+  42: '0x0000000000000000000000000000000000000000',
 }
 const usdcEthAddresses = {
-  '1' : '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc',
-  '42' : '0x0000000000000000000000000000000000000000',
-}
-const daiEthAddresses = {
-  '1' : '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11',
-  '42' : '0x0000000000000000000000000000000000000000',
+  1: '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc',
+  42: '0x0000000000000000000000000000000000000000',
 }
 
 const func = async function ({ deployments, getNamedAccounts, getChainId }) {
-  const { deploy, read, execute } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const { deploy, execute } = deployments
+  const { deployer } = await getNamedAccounts()
   const chainId = await getChainId()
 
   //const uniswapTokens0Decimals = [18, 6, 18]                // ETH/USDT, USDC/ETH, DAI/ETH.  See OurUniswapV2TWAPOracle
   //const uniswapTokens1Decimals = [6, 18, 18]                // See UniswapMedianOracle
   //const uniswapTokensInReverseOrder = [false, true, true]   // See UniswapMedianOracle
-  const usdcDecimals = 6                                      // See UniswapMedianOracle
-  const ethDecimals = 18                                      // See UniswapMedianOracle
-  const uniswapTokensInReverseOrder = true                    // See UniswapMedianOracle
+  const usdcDecimals = 6 // See UniswapMedianOracle
+  const ethDecimals = 18 // See UniswapMedianOracle
+  const uniswapTokensInReverseOrder = true // See UniswapMedianOracle
 
   let aggregator, anchoredView, usdcEthPair
   let aggregatorAddress, anchoredViewAddress, usdcEthPairAddress
 
-  if (chainId === '31337') { // buidlerevm's chainId
+  if (chainId === '31337') {
+    // buidlerevm's chainId
     const chainlinkPrice = '38598000000' // 8 dec places: see ChainlinkOracle
     const compoundPrice = '414174999' // 6 dec places: see CompoundOpenOracle
     const usdcEthCumPrice0 = '307631784275278277546624451305316303382174855535226'
@@ -44,25 +36,25 @@ const func = async function ({ deployments, getNamedAccounts, getChainId }) {
     aggregator = await deploy('MockChainlinkAggregatorV3', {
       from: deployer,
       deterministicDeployment: true,
-    });
+    })
     await execute('MockChainlinkAggregatorV3', { from: deployer }, 'set', chainlinkPrice)
-    console.log(`Deployed MockChainlinkAggregatorV3 to ${aggregator.address}`);
+    console.log(`Deployed MockChainlinkAggregatorV3 to ${aggregator.address}`)
     aggregatorAddress = aggregator.address
 
     anchoredView = await deploy('MockUniswapAnchoredView', {
       from: deployer,
       deterministicDeployment: true,
-    });
+    })
     await execute('MockUniswapAnchoredView', { from: deployer }, 'set', compoundPrice)
-    console.log(`Deployed MockUniswapAnchoredView to ${anchoredView.address}`);
+    console.log(`Deployed MockUniswapAnchoredView to ${anchoredView.address}`)
     anchoredViewAddress = anchoredView.address
 
     usdcEthPair = await deploy('MockUniswapV2Pair', {
       from: deployer,
       deterministicDeployment: true,
-    });
+    })
     await execute('MockUniswapV2Pair', { from: deployer }, 'setCumulativePrices', usdcEthCumPrice0, usdcEthCumPrice1)
-    console.log(`Deployed MockUniswapV2Pair to ${usdcEthPair.address}`);
+    console.log(`Deployed MockUniswapV2Pair to ${usdcEthPair.address}`)
     usdcEthPairAddress = usdcEthPair.address
   } else {
     aggregatorAddress = chainlinkAddresses[chainId]
@@ -74,13 +66,16 @@ const func = async function ({ deployments, getNamedAccounts, getChainId }) {
     from: deployer,
     deterministicDeployment: true,
     args: [
-      aggregatorAddress, anchoredViewAddress, usdcEthPairAddress,
-      usdcDecimals, ethDecimals,
-      uniswapTokensInReverseOrder
-    ]
+      aggregatorAddress,
+      anchoredViewAddress,
+      usdcEthPairAddress,
+      usdcDecimals,
+      ethDecimals,
+      uniswapTokensInReverseOrder,
+    ],
   })
-  console.log(`Deployed USM to ${usm.address}`);
-};
+  console.log(`Deployed USM to ${usm.address}`)
+}
 
-module.exports = func;
-module.exports.tags = ["USM"];
+module.exports = func
+module.exports.tags = ['USM']
--- deploy/2_deploy_proxy.js
+++ deploy/2_deploy_proxy.js
@@ -1,37 +1,36 @@
-
 const func = async function ({ deployments, getNamedAccounts, getChainId }) {
-  const { deploy, get, read, execute } = deployments;
-  const { deployer } = await getNamedAccounts();
+  const { deploy, get } = deployments
+  const { deployer } = await getNamedAccounts()
   const chainId = await getChainId()
 
   const wethAddresses = {
-    '1' : '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    '42' : '0xa1C74a9A3e59ffe9bEe7b85Cd6E91C0751289EbD',
+    1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    42: '0xa1C74a9A3e59ffe9bEe7b85Cd6E91C0751289EbD',
   }
-  
-  let wethAddress, usmAddress
 
-  if (chainId === '31337') { // buidlerevm's chainId
+  let wethAddress, usmAddress, weth
+
+  if (chainId === '31337') {
+    // buidlerevm's chainId
     weth = await deploy('WETH9', {
       from: deployer,
-      deterministicDeployment: true
+      deterministicDeployment: true,
     })
     wethAddress = weth.address
-  }
-  else {
+  } else {
     wethAddress = wethAddresses[chainId]
   }
 
-  usmAddress = (await get('USM')).address;
+  usmAddress = (await get('USM')).address
 
   const proxy = await deploy('Proxy', {
     from: deployer,
     deterministicDeployment: true,
-    args: [usmAddress, wethAddress]
+    args: [usmAddress, wethAddress],
   })
 
-  console.log(`Deployed Proxy to ${proxy.address}`);
+  console.log(`Deployed Proxy to ${proxy.address}`)
 }
 
-module.exports = func;
-module.exports.tags = ["Proxy"];
+module.exports = func
+module.exports.tags = ['Proxy']
