0a8be95a236a9348a2191d00e1d7f3fbdd2eaf84
Jacob Eliosoff <idkiigmy@gmail.com>
2021-02-03 03:49:06 -0500

    Major change, with 2 main components: 1. Instead of storing minFumBuyPrice when we're underwater, just store the *time* at which we went underwater, which I realized is all we need to infer the minFumBuyPrice.  This is also a behavioral change: formerly, the FUM buy price could *suddenly drop* once debt ratio went back below the max (ugly that buying FUM caused FUM price to drop - bad incentives); now it behaves continuously across that threshold - nicer. 2. I also changed how we manage stored state: packing all our stored vars into one 256-bit word, loading it at the start of each external function call, and saving all changes in one shot at the end.  Saves a bit of gas (~2k-5k per call) - may be able to tighten that up further, haven't really tried. I tried to separate these into two commits but it was too hard...  Minimal changes to the tests here (which still pass), so that's encouraging that not much behavior is changed.

--- contracts/USMView.sol
+++ contracts/USMView.sol
@@ -70,7 +70,12 @@ contract USMView {
      */
     function fumPrice(IUSM.Side side) external view returns (uint price) {
         (uint ethUsdPrice, ) = usm.latestPrice();
-        price = usm.fumPrice(side, ethUsdPrice, usm.ethPool(), usm.usmTotalSupply(), usm.fumTotalSupply(),
-                             usm.buySellAdjustment());
+        uint ethPool_ = usm.ethPool();
+        uint usmSupply = usm.usmTotalSupply();
+        uint oldTimeUnderwater = usm.timeSystemWentUnderwater();
+        if (side == IUSM.Side.Buy) {
+            (, usmSupply) = usm.checkIfUnderwater(usmSupply, ethPool_, ethUsdPrice, oldTimeUnderwater);
+        }
+        price = usm.fumPrice(side, ethUsdPrice, ethPool_, usmSupply, usm.fumTotalSupply(), usm.buySellAdjustment());
     }
 }
