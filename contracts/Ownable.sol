8343edaa17fb5c0a89b6f5faad17ab186087a4b4
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-03-17 09:24:09 +0000

    tweak: _owner

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -16,7 +16,7 @@ pragma solidity ^0.8.0;
  * the owner.
  */
 abstract contract Ownable {
-    address private _owner;
+    address public owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
@@ -24,22 +24,15 @@ abstract contract Ownable {
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
     constructor () {
-        _owner = msg.sender;
+        owner = msg.sender;
         emit OwnershipTransferred(address(0), msg.sender);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
     }
 
     /**
      * @dev Throws if called by any account other than the owner.
      */
     modifier onlyOwner() {
-        require(_owner == msg.sender, "Ownable: caller is not the owner");
+        require(owner == msg.sender, "Ownable: caller is not the owner");
         _;
     }
 
@@ -51,8 +44,8 @@ abstract contract Ownable {
      * thereby removing any functionality that is only available to the owner.
      */
     function renounceOwnership() public virtual onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
+        emit OwnershipTransferred(owner, address(0));
+        owner = address(0);
     }
 
     /**
@@ -61,7 +54,7 @@ abstract contract Ownable {
      */
     function transferOwnership(address newOwner) public virtual onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
     }
 }
