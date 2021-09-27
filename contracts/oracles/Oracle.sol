9143e9fc27ee3947cc11e2f178f4dd4903135a53
Jacob Eliosoff <idkiigmy@gmail.com>
2021-09-27 07:09:41 -0400

    Removed unused oracles: CompoundOpenOracle, UniswapV2TWAPOracle.

--- contracts/oracles/Oracle.sol
+++ contracts/oracles/Oracle.sol
@@ -6,6 +6,13 @@ abstract contract Oracle {
      * @notice Doesn't refresh the price, but returns the latest value available without doing any transactional operations:
      * eg, the price cached by the most recent call to `refreshPrice()`.
      * @return price WAD-scaled - 18 dec places
+     * @return updateTime The last time the price was updated.  This is a bit subtle: `price` can change without `updateTime`
+     * changing.  Suppose, eg, the current Uniswap v3 TWAP price over the last 10 minutes is $3,000, and then Uniswap records a
+     * new trade observation at a price of $3,020.  This will increase the 10-minute TWAP slightly above $3,000, and reset
+     * `updateTime` to the new trade's time.  But, over the subsequent 10 minutes the 10-minute TWAP will continue to increase,
+     * as more and more of "the last 10 minutes" is at $3,020 rather than $3,000: but because this gradual price change is only
+     * driven by the passage of time, not by any new price observations, it will *not* change `updateTime`, which will remain
+     * at the time of the $3,020 trade until another trade occurs.
      */
     function latestPrice() public virtual view returns (uint price, uint updateTime);
 
--- test/01_TestOracle.test.js
+++ test/01_TestOracle.test.js
@@ -5,12 +5,6 @@ const TestOracle = artifacts.require('TestOracle')
 const Aggregator = artifacts.require('MockChainlinkAggregatorV3')
 const ChainlinkOracle = artifacts.require('ChainlinkOracle')
 
-const CompoundUniswapAnchoredView = artifacts.require('MockCompoundUniswapAnchoredView')
-const CompoundOracle = artifacts.require('CompoundOpenOracle')
-
-const UniswapV2Pair = artifacts.require('MockUniswapV2Pair')
-const UniswapV2TWAPOracle = artifacts.require('UniswapV2TWAPOracle')
-
 const UniswapV3Pool = artifacts.require('MockUniswapV3Pool')
 const UniswapV3TWAPOracle = artifacts.require('UniswapV3TWAPOracle')
 
@@ -29,18 +23,10 @@ contract('Oracle pricing', (accounts) => {
   const chainlinkPriceWAD = new BN(chainlinkPrice + '0000000000') // We want 18 dec places, so add 10 0s
   const chainlinkTime = '1613550219'                        // Timestamp ("updatedAt") of the Chainlink price
 
-  const compoundPrice = '414174999'                         // CompoundUniswapAnchoredView stores 6 dec places
-  const compoundPriceWAD = new BN(compoundPrice + '000000000000') // We want 18 dec places, so add 12 0s
-
   const uniswapCumPriceScaleFactor = (new BN(2)).pow(new BN(112))
 
   const ethUsdtTokenToPrice = new BN(0)
   const ethUsdtDecimals = new BN(-12)
-  const ethUsdtV2CumPrice0_0 = new BN('30197009659458262808281833965635') // From the V2 ETH/USDT pair, see UniswapV2TWAPOracle
-  const ethUsdtV2Timestamp_0 = new BN('1606780564')
-  const ethUsdtV2CumPrice0_1 = new BN('30198349396553956234684790868151')
-  const ethUsdtV2Timestamp_1 = new BN('1606781184')
-  const ethUsdtV2ScaleFactor = uniswapCumPriceScaleFactor.div(TEN.pow(ethUsdtDecimals.neg()))
   const ethUsdtV3Timestamp0 = new BN('1632453299')
   const ethUsdtV3TickCum0 = new BN('-2406882279470') // From the V3 ETH/USDT pool
   const ethUsdtV3Timestamp1 = new BN('1632453899')
@@ -48,23 +34,10 @@ contract('Oracle pricing', (accounts) => {
 
   const usdcEthTokenToPrice = new BN(1)
   const usdcEthDecimals = new BN(-12)
-  const usdcEthV2CumPrice1_0 = new BN('31375939132666967530700283664103') // From the V2 USDC/ETH pair
-  const usdcEthV2Timestamp_0 = new BN('1606780664')
-  const usdcEthV2CumPrice1_1 = new BN('31378725947216452626380862836246')
-  const usdcEthV2Timestamp_1 = new BN('1606782003')
-  const usdcEthV2ScaleFactor = uniswapCumPriceScaleFactor.div(TEN.pow(usdcEthDecimals.neg()))
   const usdcEthV3Timestamp0 = new BN('1632203136')
   const usdcEthV3TickCum0 = new BN('2357826690419') // From the V3 USDC/ETH pool
   const usdcEthV3Timestamp1 = new BN('1632203736')
   const usdcEthV3TickCum1 = new BN('2357944474677')
-
-  const daiEthTokenToPrice = new BN(1)
-  const daiEthDecimals = new BN(0)
-  const daiEthV2CumPrice1_0 = new BN('30339833685805974401597062880404438583745289') // From the V2 DAI/ETH pair
-  const daiEthV2Timestamp_0 = new BN('1606780728')
-  const daiEthV2CumPrice1_1 = new BN('30340852730044753766501469633003499944051151')
-  const daiEthV2Timestamp_1 = new BN('1606781448')
-  const daiEthV2ScaleFactor = uniswapCumPriceScaleFactor.div(TEN.pow(daiEthDecimals.neg()))
 
   function median(a, b, c) {
     const ab = a.gt(b)      // Not "a > b", which seems to do a lexicographic comparison.  JavaScript numbers man, FML...
@@ -129,63 +102,6 @@ contract('Oracle pricing', (accounts) => {
     })
   })
 
-  describe("with CompoundOracle", () => {
-    const [deployer] = accounts
-    let oracle
-    let anchoredView
-
-    beforeEach(async () => {
-      anchoredView = await CompoundUniswapAnchoredView.new({ from: deployer })
-      await anchoredView.set(compoundPrice)
-
-      oracle = await CompoundOracle.new(anchoredView.address, { from: deployer })
-      oracle = await GasMeasuredOracleWrapper.new(oracle.address, "compound", { from: deployer })
-      await oracle.refreshPrice()
-    })
-
-    it('returns the correct price', async () => {
-      const oraclePrice = (await oracle.latestPrice())[0]
-      oraclePrice.toString().should.equal(compoundPriceWAD.toString())
-    })
-
-    it('returns the price in a transaction', async () => {
-      const oraclePrice = (await oracle.latestPrice())[0]
-    })
-  })
-
-  describe("with UniswapV2TWAPOracle", () => {
-    const [deployer] = accounts
-    let oracle
-    let pair
-
-    beforeEach(async () => {
-      pair = await UniswapV2Pair.new({ from: deployer })
-      await pair.setReserves(0, 0, ethUsdtV2Timestamp_0)
-      await pair.setCumulativePrices(ethUsdtV2CumPrice0_0, 0)
-
-      oracle = await UniswapV2TWAPOracle.new(pair.address, ethUsdtTokenToPrice, ethUsdtDecimals, { from: deployer })
-      oracle = await GasMeasuredOracleWrapper.new(oracle.address, "uniswapV2TWAP", { from: deployer })
-      await oracle.refreshPrice()
-
-      await pair.setReserves(0, 0, ethUsdtV2Timestamp_1)
-      await pair.setCumulativePrices(ethUsdtV2CumPrice0_1, 0)
-      await oracle.refreshPrice()
-    })
-
-    it('returns the correct price', async () => {
-      const oraclePrice1 = (await oracle.latestPrice())[0]
-
-      const targetOraclePriceNum = (ethUsdtV2CumPrice0_1.sub(ethUsdtV2CumPrice0_0)).mul(WAD)
-      const targetOraclePriceDenom = (ethUsdtV2Timestamp_1.sub(ethUsdtV2Timestamp_0)).mul(ethUsdtV2ScaleFactor)
-      const targetOraclePrice1 = targetOraclePriceNum.div(targetOraclePriceDenom)
-      shouldEqualApprox(oraclePrice1, targetOraclePrice1)
-    })
-
-    it('returns the price in a transaction', async () => {
-      const oraclePrice = (await oracle.latestPrice())[0]
-    })
-  })
-
   describe("with UniswapV3TWAPOracle", () => {
     const [deployer] = accounts
     let oracle
@@ -224,7 +140,7 @@ contract('Oracle pricing', (accounts) => {
   describe("with MedianOracle", () => {
     const [deployer] = accounts
     let rawOracle, oracle
-    let chainlinkAggregator, compoundView, usdcEthPair
+    let chainlinkAggregator, usdcEthPair
 
     beforeEach(async () => {
       chainlinkAggregator = await Aggregator.new({ from: deployer })
